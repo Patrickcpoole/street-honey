2b07c85b418413d1024fed0984a045d0703b8ece
patrickpoole <patrickcpoole@gmail.com>
2023-05-19 11:36:18 -0600

    updated getStrip env

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 // dev deploy
 const stripeSecretKey = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY : process.env.NEXT_PUBLIC_STRIPE_TEST_SECRET_KEY;
+console.log('stripe secrete key', stripeSecretKey)
 const stripe = new Stripe(stripeSecretKey);
 
 export default async function handler(req, res) {
